27101747f0e935afd0ffe95c41a595d6e60642e9
Josh <josh.ballard1983@gmail.com>
2025-05-24 23:58:16 -0400

    ok ready to start actually doing stiff
    Ill start by making a branch to make the home page

--- routes.py
+++ routes.py
@@ -4,7 +4,6 @@ from app import app
 
 @app.route('/')
 def home():
-	#if 'user_id' in session:
-		#return render_template('home_logged_in.html')
-	#return render_template('home_guest.html')
-	return 'Hello World'
+	if 'user_id' in session:
+		return render_template('home_logged_in.html')
+	return render_template('home_guest.html')
