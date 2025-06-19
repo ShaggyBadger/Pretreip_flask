f2c9d9875b0458374375d0bf6a1741b5da359ca1
Josh <josh.ballard1983@gmail.com>
2025-06-19 09:49:41 -0400

    Fixed secret key again. This is tough

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -2,7 +2,6 @@ from flask_app import settings
 from flask_app import app_constructor
 from flask_app import models
 from flask_app import routes
-from flask_app import secretKey
 
 __version__ = 0.1
 __author__ = 'The Bulk Fuel Ranger'
