7083ceb9ea6736e4144df228d00a14e586bfa525
Josh <josh.ballard1983@gmail.com>
2025-07-12 03:37:27 -0400

    c

--- speedGauge_app/__init__.py
+++ speedGauge_app/__init__.py
@@ -2,4 +2,3 @@ from . import SpeedgaugeApi
 from . import DbAudit
 from . import sgProcessor
 from . import dbManagement
-from . import analytics
--- speedGauge_app/analytics.py
+++ speedGauge_app/analytics.py
@@ -1,8 +1,23 @@
 import math
 
-class Analytics:
+class CompanyAnalytics:
     def __init__(self):
         self.data = []
         print('analytics initialized')
+    
+    def get_date_list(self):
+        pass
+    
+    def build_analysis_package(self, date, max_spd):
+        query = '''
+        SELECT start_date, AVG(percent_speeding)
+        FROM speedGauge_data
+        WHERE start_date BETWEEN DATE_SUB(%s, INTERVAL 1 YEAR) AND %s
+            AND percent_speeding <= %s
+        GROUP BY start_date
+        ORDER BY start_date ASC
+        '''
+    
+    
 
     
