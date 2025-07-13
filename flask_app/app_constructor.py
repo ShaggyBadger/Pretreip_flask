32294ca974bea535359cb0fefcbad4eda5c65dbf
shaggyBadger <josh.ballard1983@gmail.com>
2025-07-13 13:58:57 -0400

    more logging stuff

--- flask_app/app_constructor.py
+++ flask_app/app_constructor.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, session
 from flask_app.settings import SECRET_KEY
 from flask_app import models
 import datetime
@@ -10,36 +10,38 @@ app.db_model = db_model
 
 app.config["SECRET_KEY"] = SECRET_KEY
 
-@app.before_request
-def log_request():
-    # Get details from the request
+@app.after_request
+def log_request(response):
     ip_address = request.remote_addr
     path = request.path
     method = request.method
     user_agent = request.headers.get('User-Agent', '')
+    username = session.get('username', 'anonymous')
+    status_code = response.status_code
 
-    # get out of here if the path is lame
+    # Skip static files and favicon
     if path.startswith('/static') or path.endswith('.ico'):
-        return
+        return response
 
-
-    # Insert into DB
     query = """
-        INSERT INTO visit_log_table (ip_address, path, method, user_agent)
-        VALUES (%s, %s, %s, %s)
+        INSERT INTO visit_log_table (ip_address, path, method, user_agent, username, status_code)
+        VALUES (%s, %s, %s, %s, %s, %s)
     """
-    values = (ip_address, path, method, user_agent)
-    
+    values = (ip_address, path, method, user_agent, username, status_code)
+
     try:
         conn = app.db_model.get_db_connection()
         c = conn.cursor()
         c.execute(query, values)
         conn.commit()
     except Exception as e:
+        # Optional: log this failure somewhere else
         pass
     finally:
         c.close()
         conn.close()
+
+    return response  # Must return the response!
 
 # gotta import routes. idk why, you just do.
 import flask_app.routes
