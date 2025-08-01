4991179276e74b1353aeea078b64f4a4d6654da4
shaggyBadger <josh.ballard1983@gmail.com>
2025-07-31 23:19:32 -0400

    finished estimator!

--- tankGauge_app/routes.py
+++ tankGauge_app/routes.py
@@ -124,4 +124,109 @@ def delivery():
     if "user_id" not in flask_session:
         return redirect(url_for("home"))
     
-    return render_template(url_for('tankGauge.delivery'))
+    return render_template('tankGauge/delivery.html')
+
+@tankGauge_bp.route('/delivery-submit', methods=['POST'])
+def delivery_submit():
+    if "user_id" not in flask_session:
+        return redirect(url_for("home"))
+    
+    store_number = request.form.get("store_number")
+    fuel_types = request.form.getlist("fuel_types")  # Gets all selected checkboxes
+
+    if not store_number or not fuel_types:
+        return redirect(url_for('tankGauge.planning_selection'))
+
+    # get the store_data object for the usere's store_num entry
+    session = next(fetch_session())
+    store_data = session.query(StoreData).filter(
+    or_(
+        StoreData.store_num == store_number,
+        StoreData.riso_num == store_number
+    )
+    ).first()  # Use first() since only one store expected
+
+    # get store_tank_map model obj
+    tank_dict_list = []
+    for fuel_type in fuel_types:
+        tank_map_entry = (
+        session.query(StoreTankMap)
+        .filter_by(store_id=store_data.id, fuel_type=fuel_type)
+        .all()
+        )
+
+        if tank_map_entry:
+            for stm in tank_map_entry: # stm = StoreTankMap object
+                tank_id = stm.tank_id
+
+                # build tank chart for this tank
+                query = session.query(func.max(TankCharts.gallons))
+                query = query.filter(TankCharts.tank_type_id == tank_id)
+                max_gal = query.first()[0]
+                ninety_percent = math.floor(max_gal * 0.9)
+
+                temp_dict = {
+                    'tank_type_id': tank_id,
+                    'store_num': store_number,
+                    'fuel_type': fuel_type,
+                    'max_gal': max_gal,
+                    'ninety_percent': ninety_percent
+                }
+
+                tank_dict_list.append(temp_dict)
+
+    session.close()
+
+    return render_template(
+        'tankGauge/delivery-submit.html',
+        tanks=tank_dict_list,
+        store_num=store_number
+    )
+
+@tankGauge_bp.route('/estimate_delivery_values', methods=['POST'])
+def estimate_delivery_values():
+    if "user_id" not in flask_session:
+        return abort(404)
+    
+    # get the data that ajax is sending
+    data = request.get_json()
+    deliver_gallons = data.get("delivery_gallons")
+    tank_inches = data.get("current_inches")
+    tank_type_id = data.get('tank_type_id')
+    max_gal = data.get('max_gal')
+
+    # hit up the db
+    session = next(fetch_session())
+    
+    query = session.query(TankCharts.gallons)
+    query = query.filter(TankCharts.tank_type_id == tank_type_id)
+    query = query.filter(TankCharts.inches == tank_inches)
+    gallons_in_tank = query.first()
+    
+    try:
+        final_gal = gallons_in_tank[0] + deliver_gallons
+        query = session.query(TankCharts.inches)
+        query = query.filter(TankCharts.tank_type_id == tank_type_id)
+        query = query.filter(TankCharts.gallons <= final_gal)
+        query = query.order_by(TankCharts.inches.desc())
+        final_inch = query.first()[0]
+
+        return jsonify(
+            {
+                'final_inches': final_inch,
+                'final_gallons': final_gal,
+                'starting_gallons': gallons_in_tank[0],
+                'starting_inches': tank_inches
+            }
+        )
+    
+    except:
+        return jsonify(
+            {
+                "inch": None,
+                'gal': None
+            }
+        )
+
+    finally:
+        session.close()
